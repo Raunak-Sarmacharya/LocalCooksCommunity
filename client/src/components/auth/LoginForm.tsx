086644f1b17a06ab6334373c1d7f3d63bb701ec9
Satyajit Debnath <90326042+satyajitdebnath13@users.noreply.github.com>
2025-06-13 02:37:49 +0530

    Refactor LoginForm to use inline SVG for Google icon and update AuthPage to handle loading state
    This commit replaces the Google icon in the LoginForm component with an inline SVG for improved performance and accessibility. Additionally, the AuthPage component is updated to manage loading states more effectively, displaying a loading message while user authentication is in progress.

--- client/src/components/auth/LoginForm.tsx
+++ client/src/components/auth/LoginForm.tsx
@@ -76,7 +76,16 @@ export default function LoginForm({ onSuccess }: LoginFormProps) {
         disabled={loading}
         aria-label="Continue with Google"
       >
-        <img src="/google-icon.svg" alt="Google" className="w-5 h-5" />
+        {/* Inline Google SVG icon from the internet */}
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 48 48">
+          <g>
+            <path fill="#4285F4" d="M24 9.5c3.54 0 6.7 1.22 9.19 3.23l6.85-6.85C35.64 2.36 30.18 0 24 0 14.82 0 6.73 5.48 2.69 13.44l7.98 6.2C12.13 13.13 17.62 9.5 24 9.5z"/>
+            <path fill="#34A853" d="M46.1 24.55c0-1.64-.15-3.22-.43-4.74H24v9.01h12.42c-.54 2.9-2.18 5.36-4.65 7.01l7.19 5.6C43.98 37.36 46.1 31.44 46.1 24.55z"/>
+            <path fill="#FBBC05" d="M10.67 28.65c-1.01-2.99-1.01-6.31 0-9.3l-7.98-6.2C.99 17.36 0 20.57 0 24c0 3.43.99 6.64 2.69 9.44l7.98-6.2z"/>
+            <path fill="#EA4335" d="M24 48c6.18 0 11.64-2.04 15.54-5.56l-7.19-5.6c-2.01 1.35-4.59 2.16-8.35 2.16-6.38 0-11.87-3.63-14.33-8.94l-7.98 6.2C6.73 42.52 14.82 48 24 48z"/>
+            <path fill="none" d="M0 0h48v48H0z"/>
+          </g>
+        </svg>
         Continue with Google
       </Button>
 
